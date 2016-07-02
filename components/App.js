a79689076efa5706738530bb3b13669f6c39db12
Brandon Ramirez <brandon_ramirez@skillsoft.com>
2016-07-02 15:06:45 -0400

    Finished part 8.

--- components/App.js
+++ components/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react'
-import TextInput from './TextInput'
+import React, { Component } from 'react';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+import { connect } from 'react-redux';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <h1>This is the App Component</h1>
-        <TextInput/>
+        <h1>Todo List</h1>
+        <TodoInput dispatch={this.props.dispatch} />
+        <TodoList todos={this.props.todos} />
       </div>
-    )
+    );
   }
 
+};
+
+function mapStateToProps(state) {
+  return state;
 }
 
-export default App
+export default connect(mapStateToProps)(App);
