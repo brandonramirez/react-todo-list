87f02756c3e6d36f1654ee6758f05b810ecec8f1
Brandon Ramirez <brandon_ramirez@skillsoft.com>
2016-07-12 09:10:38 -0400

    End of part 10.

--- redux/actions.js
+++ redux/actions.js
@@ -18,6 +18,12 @@ let actions = {
       type: 'DELETE_TODO',
       id: id
     }
+  },
+  createNewUserId: function () {
+    return {
+      type: 'CREATE_USER_ID',
+      id: Math.round(Math.random() * 10)
+    };
   }
 
 }
--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-import reducer from './reducer'
+import reducer from './reducers'
 import logger from 'redux-logger'
 
 let finalCreateStore = compose(
@@ -7,6 +7,6 @@ let finalCreateStore = compose(
 )(createStore)
 
 
-export default function configureStore(initialState = { todos: [] }) {
+export default function configureStore(initialState = { todos: [], user: {} }) {
   return finalCreateStore(reducer, initialState)
 }
